d500a013785800afadca24dbdec1572589c379c8
ivanboz1014 <ivanboz1014@gmail.com>
2023-02-07 20:12:57 -0600

    class21

---
+++ src/class21/Animal.java
@@ -0,0 +1,30 @@
+package class21;
+
+
+
+    public class Animal {
+        String name;
+        String color="Black";
+
+    }
+
+    class Cat extends Animal{
+        String color="white";
+        int age=10;
+        double weight;
+
+        void printColor(){
+            String color="Blue";
+            System.out.println(color);
+        }
+
+    }
+    class AnimalTester{
+        public static void main(String[] args) {
+
+            Cat cat=new Cat();
+            System.out.println(cat.color);
+        }
+    }
+
+
---
+++ src/class21/Programming.java
@@ -0,0 +1,16 @@
+package class21;
+
+public class Programming {
+
+    Programming(){
+        System.out.println("I love programming");
+    }
+
+    Programming(String word){
+        System.out.println("I love programming "+word);
+    }
+
+    public static void main(String[] args) {
+         new Programming("Java");
+    }
+}
