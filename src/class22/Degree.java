0e625465d181dff40a531785fcb379c30c72e281
ivanboz1014 <ivanboz1014@gmail.com>
2023-02-08 20:54:29 -0600

    class22

---
+++ src/class22/Degree.java
@@ -0,0 +1,39 @@
+package class22;
+
+public class Degree {
+
+    void getPrerequisite(){
+        System.out.println("To get a degree you need high school diploma");
+
+    }}
+
+    class Bachelors extends Degree{
+
+    }
+
+
+
+    class Masters extends Degree{
+
+        @Override
+        void getPrerequisite() {
+            System.out.println("To get masters we need bachelor degree");
+        }
+    }
+
+class DegreeTester{
+    public static void main(String[] args) {
+        Degree degree=new Degree();
+        degree.getPrerequisite();
+
+        Bachelors bachelors=new Bachelors();
+        bachelors.getPrerequisite();
+
+        Masters masters=new Masters();
+        masters.getPrerequisite();
+
+
+
+
+    }
+}
