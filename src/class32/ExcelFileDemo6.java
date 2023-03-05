ddead98b0a3576a490409103564cf2c7b75572ff
ivanboz1014 <ivanboz1014@gmail.com>
2023-03-05 13:48:17 -0600

    class32

---
+++ src/class32/ExcelFileDemo6.java
@@ -0,0 +1,41 @@
+package class32;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ExcelFileDemo6 {
+    public static void main(String[] args) throws IOException {
+
+        String path="Files/Book11.xlsx";
+        FileInputStream fileInputStream=new FileInputStream(path);
+        XSSFWorkbook xssfWorkbook=new XSSFWorkbook(fileInputStream);
+        Sheet sheet2=xssfWorkbook.getSheet("Sheet2");
+        int noOfRows=sheet2.getPhysicalNumberOfRows();
+        System.out.println(noOfRows);
+
+        for (int i = 0; i <noOfRows ; i++) {
+            Row row=sheet2.getRow(i);
+
+            int noOfCells=row.getPhysicalNumberOfCells();
+            for (int j = 0; j <noOfCells ; j++) {
+                System.out.println(row.getCell(j)+" ");
+
+            }
+
+            System.out.print(row.getCell(0)+" "+row.getCell(1));
+
+
+
+        }
+        System.out.println();
+
+    }
+
+
+}
+
+
